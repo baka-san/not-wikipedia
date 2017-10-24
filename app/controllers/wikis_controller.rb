be122cfa3cd055a5ce89c1ae80b7b5b1ee7d3431
Grant <gsbackes@gmail.com>
2017-10-25 03:31:10 +0900

    Checkpoint 4: Finish wiki controller and make basic views.

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,7 +1,8 @@
 class WikisController < ApplicationController
 
-  # def index
-  # end
+  def index
+    @wikis = Wiki.all
+  end
 
   def show
     @wiki = Wiki.find(params[:id])
@@ -12,21 +13,52 @@ class WikisController < ApplicationController
   end
 
   def create
+    @wiki = Wiki.new(wiki_params)
+    @wiki.user = current_user
+
+    if @wiki.save
+      flash[:notice] = "Wiki created."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error creating the wiki. Please try again."
+      render :new
+    end
+
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
 
   def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.assign_attributes(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Page was updated. Jesus will check the validity later."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "Error saving page. Did you write something dumb? Anyways, try again when you're smarter."
+      render :edit
+    end
   end
 
   def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" is dead. Good job, you killed it."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error killing the wiki. The police will arrive shortly."
+      render :show
+    end
   end
 
   private 
 
     def wiki_params
-      params.require(:wiki).allow(:title, :body, :private)
+      params.require(:wiki).permit(:title, :body, :private)
     end
 
 end
