de816a11931cc67ab6b694fe357130998b25bcc2
Grant <gsbackes@gmail.com>
2017-11-24 14:52:53 +0900

    do some webhook stuff for successful and failed payments.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,67 +1,15 @@
 class Subscription < ApplicationRecord
+  belongs_to :user
 
+  def turn_on_autorenewal
+    # customer = Stripe::Customer.retrieve(self.user.stripe_customer_id)
+    sub = Stripe::Subscription.retrieve(stripe_subscription_id)
+    sub.cancel_at_period_end = false
+  end
 
-
-
-  # def subscribe_to_wiki()
-  #   create_subscription(find_customer)
-  # end
-
-  # private
-
-  #   def find_customer
-  #     if @user.stripe_customer_id
-  #       retrieve_customer(@user.stripe_customer_id)
-  #     else
-  #       create_customer
-  #     end
-  #   end
-
-  #   def retrieve_customer(id)
-  #     Stripe::Customer.retrieve(id) 
-  #   end
-
-  #   def create_customer
-  #     # Create customer in Stripe
-  #     customer = Stripe::Customer.create(
-  #       email: @email,
-  #       description: @email,
-
-  #       source: @token
-  #     )
-
-  #     # Update ActiveRecord
-  #     @user.update(stripe_customer_id: customer.id)
-  #     customer
-  #   end
-
-  #   def create_subscription(customer)
-  #     byebug
-
-  #     # Create subscription in Stripe for the current_user
-  #     subscription = Stripe::Subscription.create(
-  #       customer: customer.id,
-  #       :items => [
-  #         {
-  #           :plan => "premium"
-  #         },
-  #       ]
-  #     )
-
-  #     # Update ActiveRecord
-  #     sub = Subscription.create!(
-  #       user_id: @user.id, 
-  #       stripe_subscription_id: subscription.id,
-  #       current_period_start: subscription.current_period_start,
-  #       current_period_end: subscription.current_period_end
-  #     )
-  #     # sub.update(
-  #     #   current_period_start: subscription.current_period_start,
-  #     #   current_period_end: subscription.current_period_end
-  #     # )
-
-  #     subscription
-  #   end
+  def turn_off_autorenewal
+    sub = Stripe::Subscription.retrieve(stripe_subscription_id)
+    sub.cancel_at_period_end = true
+  end
   
-
 end
--- app/services/subscriptions_service.rb
+++ app/services/subscriptions_service.rb
@@ -11,32 +11,26 @@ class SubscriptionsService
     @token = params[:stripeToken]
   end
 
-
-  # How do I handle a user who cancels and then reactivates? Do I just call 
-  # create again or something? Look up Stripe method.
-  def call
-    find_subscription
+  def call_create
+    create_subscription(find_customer)
+    # find_subscription
   end
 
   private
 
-    # Why do we need attr_accessor here? Can't we just use the instance variables?
-    # attr_accessor :user, :stripe_email, :stripe_token
+    # def find_subscription
+    #   subscription = Subscription.find_by(user_id: @user.id)
 
+    #   if subscription
+    #     retrieve_subscription(subscription.stripe_subscription_id)
+    #   else
+    #     create_subscription(find_customer)
+    #   end
+    # end
 
-    def find_subscription
-      subscription = Subscription.find_by(user_id: @user.id)
-
-      if subscription
-        retrieve_subscription(subscription.stripe_subscription_id)
-      else
-        create_subscription(find_customer)
-      end
-    end
-
-    def retrieve_subscription(id)
-      Stripe::Subscription.retrieve(id)
-    end
+    # def retrieve_subscription(id)
+    #   Stripe::Subscription.retrieve(id)
+    # end
 
     def find_customer
       if @user.stripe_customer_id
