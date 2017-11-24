9c3eca531f15d67c12ecd9e045497179847721e7
Grant <gsbackes@gmail.com>
2017-11-24 14:56:03 +0900

    changes to webhooks

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -7,31 +7,45 @@ class StripeController < ApplicationController
       #refer event types here https://stripe.com/docs/api#event_types
       case event_json['type']
         when 'invoice.payment_succeeded'
-          # handle_success_invoice event_object
-          customer = User.where(stripe_customer_id: event_object.customer).first
-
           # Update subscription in ActiveRecord
           # Which date is best to use?
           # Move this to a model or service?
-          subscription = customer.subscription
-          subscription.current_period_start = subscription.current_period_end # Date.today.to_datetime
-           # date.today.to_datetime.to_i
-          subscription.current_period_end += 1.month.to_i  # Date.today.to_datetime + 1.month
-           # subscription.current_period_start + 1.month.to_i
-
-        when 'invoice.payment_failed'
-          # handle_failure_invoice event_object
-
-          # Downgrade user to standard in ActiveRecord
           customer = User.where(stripe_customer_id: event_object.customer).first
-          customer.downgrade_to_standard
+          subscription = customer.subscription
+
+          if subscription
+            subscription.current_period_start = date.today.to_datetime.to_i
+            # subscription.current_period_end
+            subscription.current_period_end = date.today.to_datetime.to_i + 1.month.to_i 
+            # subscription.current_period_start + 1.month.to_i
+            subscription.save
+          end
+
+        # when 'invoice.payment_failed'
+          # Downgrade user to standard in ActiveRecord
+          # customer = User.where(stripe_customer_id: event_object.customer).first
+          # customer.downgrade_to_standard
+
+          # Delete the subscription from ActiveRecord
 
           # Send an email??
 
-        when 'charge.failed'
+        # when 'charge.failed'
           # handle_failure_charge event_object
+
+
         when 'customer.subscription.deleted'
-        when 'customer.subscription.updated'
+          customer = User.where(stripe_customer_id: event_object.customer).first
+          subscription = customer.subscription
+
+          if subscription
+            subscription.destroy
+            subscription.save
+          end
+
+        # when 'customer.subscription.updated'
+          # handle event
+
       end
     rescue Exception => ex
       render :json => {:status => 422, :error => "Webhook call failed"}
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,8 +3,6 @@ class SubscriptionsController < ApplicationController
   protect_from_forgery :except => :webhooks
 
   def new
-    # get rid of plans model, do it on stripe
-    # search docs
     # @plan = Stripe::Plan.retrieve(id: 'premium')
     @subscription = Subscription.new
   end
@@ -17,7 +15,7 @@ class SubscriptionsController < ApplicationController
     else
       begin 
       # Create a subscription in the database and in Stripe
-      SubscriptionsService.new(subscriptions_params, current_user).call
+      SubscriptionsService.new(subscriptions_params, current_user).call_create
 
       current_user.upgrade_to_premium
 
@@ -29,13 +27,27 @@ class SubscriptionsController < ApplicationController
         redirect_back(fallback_location: root_path)
       end
       
-      # Stripe will send back CardErrors, with friendly messages when something goes wrong.
-      # This `rescue block` catches and displays those errors.
+      # Stripe will send back CardErrors, displayed in this rescue block.
       rescue Stripe::CardError => e
         flash[:alert] = e.message
         redirect_to new_subscription_path
       end
     end
+  end
+
+  def update
+    # should this be update or destroy
+    # cancel_at_period_end = true # then it will wait till the end. delete from db in webhook
+    # add a column to subscriptions for reoccuring pay true or false
+
+    # If the user already has an active subscription, don't allow them to subscribe again.
+    if current_user.standard?
+      flash[:alert] = "Hey, you need to upgrade to do that buddy!"
+      redirect_to current_user
+    else
+    # current_user.subscription.downgrade
+    end
+
   end
 
   private
