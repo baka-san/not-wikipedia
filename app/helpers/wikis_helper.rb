ced42fc50ba4ed19a02616961a5ee2d027076f96
Grant <gsbackes@gmail.com>
2017-12-31 16:34:23 +0900

    tweak a few bugs in the view

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,7 +1,7 @@
 module WikisHelper
 
   def authorized_for_this_private_wiki?
-    current_user && (current_user == @wiki.user || current_user.admin?)
+    current_user && (@wiki.owner?(current_user) || current_user.admin? || current_user.collaborating_on?(@wiki))
   end
 
 end
