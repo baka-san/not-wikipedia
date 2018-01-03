3f94ef8c6c6015870c9b474e7d4140957d121420
Grant <gsbackes@gmail.com>
2018-01-04 05:42:09 +0900

    more heroku fun

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -4,7 +4,6 @@ class CollaborationsController < ApplicationController
   def create
     respond_to do |format|
       format.js do
-        byebug
 
         @wiki = Wiki.find(collaboration_params[:wiki_id])
         @collaborator = User.find_by(email: 'collaboration_params[:email]')
